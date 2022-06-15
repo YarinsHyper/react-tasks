fc0efcb28584b0b15048af1b38560df6a6a50bd9
yarinBenisty <yarin1857@gmail.com>
2022-06-15 10:58:54 +0300

    fix: some changes

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,15 +1,13 @@
 import CartIcon from "../Cart/CartIcon";
-import classes from './HeaderCartButton.module.css';
-const HeaderCartButton = () => {
+import classes from "./HeaderCartButton.module.css";
+const HeaderCartButton = (props) => {
   return (
-    <button className={classes.button}>
+    <button className={classes.button} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
       <span>Your Cart</span>
-      <span className={classes.badge}>
-        3
-      </span>
+      <span className={classes.badge}>3</span>
     </button>
   );
 };
